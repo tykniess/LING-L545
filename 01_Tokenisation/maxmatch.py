f3289ecd723dffc7d204ed20b3c9deb403827942
Tyler Kniess <tyler@149-161-216-9.dhcp-bl.indiana.edu>
2019-10-09 14:55:45 -0400

    progress on maxmatch, still struggling with how to maxmatch the rest of a sentence after finding a match

--- 01_Tokenisation/maxmatch.py
+++ 01_Tokenisation/maxmatch.py
@@ -1,33 +1,53 @@
 import sys #needed for standard input and sys.argv
+import ssl
 from nltk import sent_tokenize #needed to tokenize
 
 ###########################
 #Stage 1: sent tokenisation
 l1 = [] #new list for lines
-l2 = [] #output list
 for line in sys.stdin:
-        for sent in sent_tokenize(line, language = 'german'):
-                l1.append(sent)
+#        for sent in sent_tokenize(line, language = 'japanese'):
+###Had to scrap tokenisation because PUNKT doesn't have japanese.pickle
+        l1.append(line)
 	
-print ("#####\nYour file is " + str(len(l1))+ " sentences long!\n#####")
 
 ###########################
 #Stage 2: word/character tokenisation (Japanese)
 
-for line in l1:
-        i=0
-        with open('ja_dict.txt','r') as dict:
-                for entry in dict:
-                        if line[:len(line)-i] in entry:
-                                l2.append(line)
-                                break #go to next line?
-                        else:
-                                i=i+1
-print(len(l2))
-for line in l2:
-        print(line)
-        
+#2a: create list of possible maxmatch types
+l2=[]
+i=0
+firstword = ''
+remainder = ''
+ja_dict = []
+with open('ja_dict.txt','r') as file:
+        for line in file:
+                ja_dict.append(line)
+print('your dictionary has ' + str(len(ja_dict)) + ' entries')
+
+for sent in l1:
+        if sent == '':
+                break
+        else:
+                i = len(sent)
+                while i > 0:
+                        firstword = sent[:i]
+                        remainder = sent[i:]
+                        print('first word is ' + firstword)
+                        print('remainder is ' + remainder)
+                        print('i is now '+str(i))
+
+                        for entry in ja_dict:
+                                if firstword in entry:
+                                        print('###we have a match')
+                                        l2.append(entry)
+                                        break
+                        i=i-1
+                break
+
+print ('your maxmatch output is: ' + str(l2))
 
 
-#adding the dictionary argument
 print ("#####\nYour script has the following arguments: " + str(sys.argv))
+
+
