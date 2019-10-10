c75c2fdc50db9647f00ba6b5dbd3dd90abd69be8
Tyler Kniess <tyler@149-161-242-88.dhcp-bl.indiana.edu>
2019-10-10 15:39:14 -0400

    fix maxmatch.py

--- 01_Tokenisation/maxmatch.py
+++ 01_Tokenisation/maxmatch.py
@@ -1,53 +1,62 @@
 import sys #needed for standard input and sys.argv
-import ssl
-from nltk import sent_tokenize #needed to tokenize
+import csv #needed to just grab surfce forms from dictionary
 
 ###########################
-#Stage 1: sent tokenisation
-l1 = [] #new list for lines
+#Stage 1: making a list from tokenized input 
+l1 = [] # list for input lines
 for line in sys.stdin:
-#        for sent in sent_tokenize(line, language = 'japanese'):
-###Had to scrap tokenisation because PUNKT doesn't have japanese.pickle
         l1.append(line)
-	
+l2 = [] # list for maxmatch output forms
 
 ###########################
-#Stage 2: word/character tokenisation (Japanese)
+input_file = "ja_dict.txt"
+output_file = input_file + "-surface_forms.txt"
 
-#2a: create list of possible maxmatch types
-l2=[]
-i=0
-firstword = ''
-remainder = ''
-ja_dict = []
-with open('ja_dict.txt','r') as file:
-        for line in file:
-                ja_dict.append(line)
-print('your dictionary has ' + str(len(ja_dict)) + ' entries')
+with open(input_file,'r') as to_read:
+        with open(output_file, 'w') as temp_file:
+                reader = csv.reader(to_read, delimiter = "\t")
+                #writer = open(temp_file, 'w')
+                desired_column = [2]
 
-for sent in l1:
-        if sent == '':
-                break
+                for row in reader:
+                        if len(row) != 10:
+                                continue
+                        else:
+                                column = row[2] #for i in desired_column)
+                                temp_file.write(column + '\n')
+
+
+def Find(word):
+        with open(output_file, 'r') as searchable:
+                for line in searchable:
+                        if word == line[:len(line)-1]:
+                                return True
+                        else:
+                                continue
+
+def MaxMatch(sentence):
+        """Takes a sentence and attempts to find maxmatch in a dictionary."""        
+        if sentence == '':
+                return False
+                print ('empty sentence')
         else:
-                i = len(sent)
+                i=len(sentence)
                 while i > 0:
-                        firstword = sent[:i]
-                        remainder = sent[i:]
-                        print('first word is ' + firstword)
-                        print('remainder is ' + remainder)
-                        print('i is now '+str(i))
+                        firstword = sentence[:i]
+                        remainder = sentence[i:]
+                        if Find(firstword):
+                                l2.append(firstword)
+                                sentence = remainder
+                                i = len(sentence)
+                        else:
+                                i=i-1
+                
+                                
+for sent in l1:
+        MaxMatch(sent)
 
-                        for entry in ja_dict:
-                                if firstword in entry:
-                                        print('###we have a match')
-                                        l2.append(entry)
-                                        break
-                        i=i-1
-                break
-
-print ('your maxmatch output is: ' + str(l2))
-
-
-print ("#####\nYour script has the following arguments: " + str(sys.argv))
-
-
+print("Your MaxMatch tokenized output is:")
+i = 1
+for maxmatch_output in l2:
+        print (str(i) + '\t' + maxmatch_output + '\n')
+        i = i + 1
