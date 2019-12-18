26c2bbd936236539bcc87a659647cb5cc53d7ae4
Tyler Kniess <tyler@149-161-242-254.dhcp-bl.indiana.edu>
2019-12-18 16:35:39 -0500

    finishing touches

--- 04_Parsing/word_order.py
+++ 04_Parsing/word_order.py
@@ -1,9 +1,12 @@
-#This script extracts information on subject-verb order from a train.conllu file
-#and returns a tuple of ( language name , number of s-v sentences, number of
-#v-s sentences )
+#This script extracts information on object-verb order from train.conllu file(s)
+#and prints the information in a legible format.
+#It was designed to be used in command line to write to a text file and to
+#create a .png visualization.
+
+#to use it, pass it any number of .conllu files in command line.
 
 #it does this by reading the file line-by-line and looking for
-#lines containing 'obj' and lines containing 'VerbForm=Fin'.
+#lines containing 'obj' determining if they point forward or backward.
 #Because the annotation includes in column 6 the head verb, we can link
 #object to verb.
 
@@ -16,11 +19,13 @@
 
 import sys
 import csv
+import matplotlib.pyplot as plt
 csv.field_size_limit(sys.maxsize) #needed to unlock the exta memory to be over 9000
-print('Language\tnum_VO\tnum_OV')
+tuples = [] #list-of-tuples to save processing time later
+print('(Language,num_VO,num_OV)')
 
-for conllu_file in sys.argv[1:]: #takes any number of .conllu arguments
-    with open(conllu_file, 'r') as csv_file: #for each .conllu file reads lines
+def count(conllu_name):
+    with open(conllu_name, 'r') as csv_file: #for each .conllu file reads lines
         csv_reader = csv.reader(csv_file, delimiter = '\t') #tab-delineated
         #figure out language name by reading until you hit a dash
         i=0
@@ -47,4 +52,34 @@ for conllu_file in sys.argv[1:]: #takes any number of .conllu arguments
                     OV_sen = OV_sen + 1
             else:
                 pass
-        print(language_name +'\t'+ str(VO_sen) +'\t'+ str(OV_sen))
+        return language_name, VO_sen, OV_sen
+
+for conllu_file in sys.argv[1:]: #takes any number of .conllu arguments
+    tuples.append(count(conllu_file))
+
+for item in tuples:
+    print(item)
+
+#now to plot. I want this baby to be able to automatically create labels and values
+labels = {}
+x = [] # percent OV
+y = [] # percent VO
+i=0 #index to work through list of tuples
+while i < len (tuples):
+    pct_OV = tuples[i][2]/(tuples[i][1]+tuples[i][2])
+    pct_VO = tuples[i][1]/(tuples[i][1]+tuples[i][2])
+    labels[i]=tuples[i][0] #take the first element of the tuple at index i in the list
+    x.append(pct_OV) #add the number of VO sentences (see language_data.txt)
+    y.append(pct_VO) #add pct_VO
+    i=i+1
+
+plt.plot(x, y, 'ro')
+plt.title('Relative word order of verb and object')
+plt.xlim([0,1]) # Set the x and y axis ranges
+plt.ylim([0,1])
+plt.xlabel('OV') # Set the x and y axis labels
+plt.ylabel('VO')
+for j in labels:  # Add labels to each of the points
+    plt.text(x[j]-0.03, y[j]-0.03, labels[j], fontsize=5.5)
+plt.savefig('languages')
+#plt.show()
